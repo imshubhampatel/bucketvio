216b67d52822b1ac8e11d5bfa2acbf1748ef350f
ardourApeX <joygupta501@gmail.com>
2021-10-21 00:55:21 +0530

    Feat : Added Server Config

---
+++ server/config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const chalk = require('chalk')
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true });
+
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error"));
+
+db.once("open", function (err) {
+    if (err) { console.log(err); }
+    console.log(chalk.bgRed.white(" Connected To Database Successfully :) "));
+});
+
+
+
